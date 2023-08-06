aba7dfb935f215054b560465b8d4895527d4b3e5
Mark Robertson <markrobertson67@gmail.com>
2023-08-06 19:05:17 -0400

    making some files

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json( {message: "Welcome to the Products App Back-end"} );
+    }
+);
+
+app.get ("*", (req, res) => {
+
+    res.status(404).json({message: "Page Not Found"});
+});
+
+
+module.exports = app;
