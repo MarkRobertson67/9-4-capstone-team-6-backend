29ca23d8662e3aee329f1470cc27b5753814c2fa
arerimr <raydelys.morrobelr@gmail.com>
2023-08-20 14:33:12 -0400

    create queries files and set up commentary queries

---
+++ Queries/commentary.js
@@ -0,0 +1,53 @@
+const db = require('../db/dbConfig')
+
+const getComments = async () => {
+    try {
+
+    } catch (e) {
+        return e
+    }
+}
+
+const getComment = async (id) => {
+    try {
+
+    } catch (e) {
+        return e;
+    }
+}
+
+const updateComment = async (id, comment) => {
+    const { } = comment
+
+    try {
+
+    } catch (e) {
+        return e;
+    }
+}
+
+const createComment = async (newComment) => {
+    const { } = newComment
+
+    try {
+
+    } catch (e) {
+        return e;
+    }
+}
+
+const destroyComment = async (id) => {
+    try {
+
+    } catch (e) {
+        return e;
+    }
+}
+
+module.exports = {
+    getComments,
+    getComment,
+    updateComment,
+    createComment,
+    destroyComment
+}
