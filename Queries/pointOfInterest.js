36d5e6f2bf119300fda05fe314a8de81b43a0fa7
arerimr <raydelys.morrobelr@gmail.com>
2023-08-20 14:35:09 -0400

    set up poi queries

---
+++ Queries/pointOfInterest.js
@@ -0,0 +1,53 @@
+const db = require('../db/dbConfig')
+
+const getPointsOfInterest = async () => {
+    try {
+
+    } catch (e) {
+        return e
+    }
+}
+
+const getPointOfInterest = async (id) => {
+    try {
+
+    } catch (e) {
+        return e;
+    }
+}
+
+const updatePointOfInterest = async (id, PointOfInterest) => {
+    const { } = PointOfInterest
+
+    try {
+
+    } catch (e) {
+        return e;
+    }
+}
+
+const createPointOfInterest = async (newPointOfInterest) => {
+    const { } = newPointOfInterest
+
+    try {
+
+    } catch (e) {
+        return e;
+    }
+}
+
+const destroyPointOfInterest = async (id) => {
+    try {
+
+    } catch (e) {
+        return e;
+    }
+}
+
+module.exports = {
+    getPointsOfInterest,
+    getPointOfInterest,
+    updatePointOfInterest,
+    createPointOfInterest,
+    destroyPointOfInterest
+}
