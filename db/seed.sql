bdb5b2ccb09f52d3693027160d7ddafa3d6f4d05
arerimr <raydelys.morrobelr@gmail.com>
2023-08-20 14:42:14 -0400

    finished sql seed set up

--- db/seed.sql
+++ db/seed.sql
@@ -10,3 +10,23 @@ VALUES
 (),
 (),
 ();
+
+
+INSERT INTO point_of_interest (id, latitude, name, tour_id, image_url, created_at)
+VALUES
+
+(),
+(),
+(),
+(),
+();
+
+
+INSERT INTO commentary (id, poi_id, name, lang_code, description, translated_description, audio_url, created_at)
+VALUES
+
+(),
+(),
+(),
+(),
+();
