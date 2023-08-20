afecd4877bcf7b5aa8f930584c506aa3abfead3e
arerimr <raydelys.morrobelr@gmail.com>
2023-08-20 17:39:39 -0400

    added tour route

--- app.js
+++ app.js
@@ -9,16 +9,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const tourController = require('./Controllers/tourController.js')
+app.use('/tours', tourController)
 
 // ROUTES
 app.get("/", (req, res) => {
-    res.json( {message: "Welcome to our Capstone App Back-end"} );
-    }
+    res.json({ message: "Welcome to our Capstone App Back-end" });
+}
 );
 
 // ROUTES
-app.get ("*", (req, res) => {
-    res.status(404).json({message: "Page Not Found"});
+app.get("*", (req, res) => {
+    res.status(404).json({ message: "Page Not Found" });
 });
 
 
