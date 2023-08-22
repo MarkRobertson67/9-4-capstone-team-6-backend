bb3e2515ea3e943550298cadf49aafdfb25a5ba8
arerimr <raydelys.morrobelr@gmail.com>
2023-08-21 20:52:13 -0400

    update commentary query

--- Queries/commentary.js
+++ Queries/commentary.js
@@ -22,7 +22,8 @@ const updateComment = async (id, comment) => {
     const { poi_id, name, lang_code, description, translated_description, audio_url, created_at } = comment
 
     try {
-
+        const updatedComment = await db.one("UPDATE commentary SET poi_id=$1, name=$2, lang_code=$3, description=$4, translated_description=$5, audio_url=$6, created_at=$7 WHERE id=$8 RETURNING *", [poi_id, name, lang_code, description, translated_description, audio_url, created_at, id])
+        return updatedComment;
     } catch (e) {
         return e;
     }
