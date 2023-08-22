0cab276b2b95e924e384df7fd03611a80d34ebfa
arerimr <raydelys.morrobelr@gmail.com>
2023-08-22 18:51:18 -0400

    imported and renamed poi query functions in poi controller

--- Controllers/pointOfInterestController.js
+++ Controllers/pointOfInterestController.js
@@ -1,51 +1,51 @@
 const express = require('express');
-const pointOfInterest = express.Router()
-const { PointOfInterest } = require('../Queries/pointOfInterest')
+const pointsOfInterest = express.Router()
+const { getPointsOfInterest, getPointOfInterest, updatePointOfInterest, createPointOfInterest, destroyPointOfInterest } = require('../Queries/pointOfInterest')
 
-pointOfInterest.get('/', async (req, res) => {
-    const allPointOfInterest = await getPointOfInterest();
-    allPointOfInterest ? res.status(200).json(allPointOfInterest) : res.status(500).json({ error: 'server error' })
+pointsOfInterest.get('/', async (req, res) => {
+  const allPointOfInterest = await getPointsOfInterest();
+  allPointOfInterest ? res.status(200).json(allPointOfInterest) : res.status(500).json({ error: 'server error' })
 });
 
-pointOfInterest.get('/:id', async (req, res) => {
-    const { id } = req.params
-    const pointOfInterest = await getAPointOfInterest(id);
-    if (pointOfInterest) {
-      res.status(200).json(pointOfInterest);
-    } else {
-      res.status(404).json({ error: "not found" });
-    }
+pointsOfInterest.get('/:id', async (req, res) => {
+  const { id } = req.params
+  const pointOfInterest = await getPointOfInterest(id);
+  if (pointOfInterest) {
+    res.status(200).json(pointOfInterest);
+  } else {
+    res.status(404).json({ error: "not found" });
+  }
 });
 
-pointOfInterest.put('/:id', async (req, res) => {
-    const { id } = req.params
-    const { pointOfInterest } = req.body;
-    try {
-        const updatedPointOfInterest = await updateAPointOfInterest(id, pointOfInterest);
-        res.status(200).json(updatedPointOfInterest);
-      } catch (error) {
-        res.status(404).json({ error: "not found" });
-      }
+pointsOfInterest.put('/:id', async (req, res) => {
+  const { id } = req.params
+  const { pointOfInterest } = req.body;
+  try {
+    const updatedPointOfInterest = await updatePointOfInterest(id, pointOfInterest);
+    res.status(200).json(updatedPointOfInterest);
+  } catch (error) {
+    res.status(404).json({ error: "not found" });
+  }
 });
 
-pointOfInterest.post('/', async (req, res) => {
-    const newPointOfInterest = req.body;
-    try {
-      const addedPointOfInterest = await addAPointOfInterest(newPointOfInterest);
-      res.status(200).json(addedPointOfInterest);
-    } catch (error) {
-      res.status(404).json({ error: "not found" });
-    }
+pointsOfInterest.post('/', async (req, res) => {
+  const newPointOfInterest = req.body;
+  try {
+    const addedPointOfInterest = await createPointOfInterest(newPointOfInterest);
+    res.status(200).json(addedPointOfInterest);
+  } catch (error) {
+    res.status(404).json({ error: "not found" });
+  }
 });
 
-pointOfInterest.delete('/:id', async (req, res) => {
-    const { id } = req.params
-    try {
-        const deletedPointOfInterest = await deleteAPointOfInterest(id);
-        res.status(200).json(deletedPointOfInterest);
-      } catch (error) {
-        res.status(404).json({ error: "not found" });
-      }
+pointsOfInterest.delete('/:id', async (req, res) => {
+  const { id } = req.params
+  try {
+    const deletedPointOfInterest = await destroyPointOfInterest(id);
+    res.status(200).json(deletedPointOfInterest);
+  } catch (error) {
+    res.status(404).json({ error: "not found" });
+  }
 });
 
-module.exports = pointOfInterest;
+module.exports = pointsOfInterest;
