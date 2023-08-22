743458922082f249a013a17417677c35f9d1a854
arerimr <raydelys.morrobelr@gmail.com>
2023-08-22 18:44:38 -0400

    imported and renamed query functions accordingly

--- Controllers/tourController.js
+++ Controllers/tourController.js
@@ -1,51 +1,51 @@
 const express = require('express');
 const tour = express.Router()
-const { getTours } = require('../Queries/tours')
+const { getTours, getTour, updateTour, createTour, destroyTour } = require('../Queries/tours')
 
 tour.get('/', async (req, res) => {
-    const allTours = await getTours();
-    allTours ? res.status(200).json(allTours) : res.status(500).json({ error: 'server error' })
+  const allTours = await getTours();
+  allTours ? res.status(200).json(allTours) : res.status(500).json({ error: 'server error' })
 });
 
 tour.get('/:id', async (req, res) => {
-    const { id } = req.params
-    const tour = await getATour(id);
-    if (tour) {
-      res.status(200).json(tour);
-    } else {
-      res.status(404).json({ error: "not found" });
-    }
+  const { id } = req.params
+  const tour = await getTour(id);
+  if (tour) {
+    res.status(200).json(tour);
+  } else {
+    res.status(404).json({ error: "not found" });
+  }
 });
 
 tour.put('/:id', async (req, res) => {
-    const { id } = req.params
-    const { tour } = req.body;
-    try {
-        const updatedTour = await updateATour(id, tour);
-        res.status(200).json(updatedTour);
-      } catch (error) {
-        res.status(404).json({ error: "not found" });
-      }
+  const { id } = req.params
+  const { tour } = req.body;
+  try {
+    const updatedTour = await updateTour(id, tour);
+    res.status(200).json(updatedTour);
+  } catch (error) {
+    res.status(404).json({ error: "not found" });
+  }
 });
 
 tour.post('/', async (req, res) => {
-    const newTour = req.body;
-    try {
-      const addedTour = await addATour(newTour);
-      res.status(200).json(addedTour);
-    } catch (error) {
-      res.status(404).json({ error: "not found" });
-    }
+  const newTour = req.body;
+  try {
+    const addedTour = await createTour(newTour);
+    res.status(200).json(addedTour);
+  } catch (error) {
+    res.status(404).json({ error: "not found" });
+  }
 });
 
 tour.delete('/:id', async (req, res) => {
-    const { id } = req.params
-    try {
-        const deletedTour = await deleteATour(id);
-        res.status(200).json(deletedTour);
-      } catch (error) {
-        res.status(404).json({ error: "not found" });
-      }
+  const { id } = req.params
+  try {
+    const deletedTour = await destroyTour(id);
+    res.status(200).json(deletedTour);
+  } catch (error) {
+    res.status(404).json({ error: "not found" });
+  }
 });
 
 module.exports = tour;
